e8ea3362d4f3ce120fe827ab8caabad88bc1fcae
ECWireless <Elliott@valtechcreative.com>
2021-03-09 16:14:36 -0700

    Add Web3Context

---
+++ src/contexts/Web3Context.tsx
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { API } from 'bnc-notify';
+import getSigner from 'libs/signer';
+import { initOnboard, initNotify } from 'libs/connector';
+import { Wallet,  API as OnboardAPI } from 'libs/faces';
+
+export const Web3Context = React.createContext({});
+
+let provider: any;
+
+export const Web3Provider: React.FC = ({ children }) => {
+  const [address, setAddress] = useState(null);
+  const [network, setNetwork] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [wallet, setWallet] = useState({});
+
+  const [onboard, setOnboard] = useState<OnboardAPI | null>(null);
+  const [notify, setNotify] = useState<API | null>(null);
+
+  const [toAddress, setToAddress] = useState('');
+
+  useEffect(() => {
+    const onboard = initOnboard({
+      address: setAddress as unknown as (address: string) => void,
+      network: setNetwork as unknown as (networkId: number) => void,
+      balance: setBalance as unknown as (balance: string) => void,
+      wallet: (wallet: Wallet) => {
+        if (wallet.provider) {
+          setWallet(wallet);
+
+          const ethersProvider = new ethers.providers.Web3Provider(wallet.provider);
+
+          provider = ethersProvider;
+
+          if (wallet.name) {
+            window.localStorage.setItem('selectedWallet', wallet.name);
+          }
+        } else {
+          provider = null;
+          setWallet({});
+        }
+      },
+    });
+
+    setOnboard(onboard);
+
+    setNotify(initNotify());
+  }, []);
+
+  useEffect(() => {
+    const previouslySelectedWallet = window.localStorage.getItem('selectedWallet');
+
+    if (previouslySelectedWallet && onboard) {
+      onboard.walletSelect(previouslySelectedWallet);
+    }
+  }, [onboard]);
+
+//   async function sendHash() {
+//     if (!toAddress) {
+//       alert('An Ethereum address to send Eth to is required.');
+//       return;
+//     }
+
+//     const signer = getSigner(provider);
+
+//     const { hash } = await signer.sendTransaction({
+//       to: toAddress,
+//       value: 1000000000000000,
+//     });
+
+
+//     const { emitter } = notify.hash(hash);
+
+//     emitter.on('txPool', (transaction) => ({
+//       // message: `Your transaction is pending, click <a href="https://rinkeby.etherscan.io/tx/${transaction.hash}" rel="noopener noreferrer" target="_blank">here</a> for more info.`,
+//       // or you could use onclick for when someone clicks on the notification itself
+//       onclick: () => window.open(`https://rinkeby.etherscan.io/tx/${transaction.hash}`),
+//     }));
+
+//     emitter.on('txSent', console.log);
+//     emitter.on('txConfirmed', console.log);
+//     emitter.on('txSpeedUp', console.log);
+//     emitter.on('txCancel', console.log);
+//     emitter.on('txFailed', console.log);
+
+//     // emitter.on("all", event => {
+//     //   console.log("ALLLLLLL", event)
+//     // })
+//   }
+
+//   async function sendInternalTransaction() {
+//     if (!toAddress) {
+//       // eslint-disable-next-line no-alert
+//       alert('An Ethereum address to send Eth to is required.');
+//       return;
+//     }
+
+//     const { hash } = await internalTransferContract.internalTransfer(toAddress, {
+//       value: 1000000000000000,
+//     });
+
+//     const { emitter } = notify.hash(hash);
+
+//     emitter.on('txSent', console.log);
+//     emitter.on('txPool', console.log);
+//     emitter.on('txConfirmed', console.log);
+//     emitter.on('txSpeedUp', console.log);
+//     emitter.on('txCancel', console.log);
+//     emitter.on('txFailed', console.log);
+//   }
+
+//   async function sendTransaction() {
+//     if (!toAddress) {
+//       alert('An Ethereum address to send Eth to is required.');
+//     }
+
+//     const signer = getSigner(provider);
+
+//     const txDetails = {
+//       to: toAddress,
+//       value: 1000000000000000,
+//     };
+
+//     const sendTransaction = () => signer.sendTransaction(txDetails).then((tx: { hash: string; }) => tx.hash);
+
+//     const gasPrice = () => provider.getGasPrice().then((res: { toString: () => any; }) => res.toString());
+
+//     const estimateGas = () => provider.estimateGas(txDetails).then((res: { toString: () => any; }) => res.toString());
+
+//     const { emitter } = await notify.transaction({
+//       sendTransaction,
+//       gasPrice,
+//       estimateGas,
+//       balance: onboard?.getState().balance,
+//       txDetails,
+//     });
+
+//     emitter.on('txRequest', console.log);
+//     emitter.on('nsfFail', console.log);
+//     emitter.on('txRepeat', console.log);
+//     emitter.on('txAwaitingApproval', console.log);
+//     emitter.on('txConfirmReminder', console.log);
+//     emitter.on('txSendFail', console.log);
+//     emitter.on('txError', console.log);
+//     emitter.on('txUnderPriced', console.log);
+//     emitter.on('txSent', console.log);
+//     emitter.on('txPool', console.log);
+//     emitter.on('txConfirmed', console.log);
+//     emitter.on('txSpeedUp', console.log);
+//     emitter.on('txCancel', console.log);
+//     emitter.on('txFailed', console.log);
+//   }
+
+  return (
+    <Web3Context.Provider
+      value={{
+        address,
+        network,
+        balance,
+        wallet,
+        onboard,
+        notify,
+        provider,
+      }}
+    >
+      {children}
+    </Web3Context.Provider>
+  );
+};
--- src/libs/connector.ts
+++ src/libs/connector.ts
@@ -1,78 +1,76 @@
+import Notify from 'bnc-notify';
 import Onboard from 'bnc-onboard';
-import Web3 from 'web3';
-import Notify, { API } from 'bnc-notify';
 import getRpcUrl from 'libs/rpc';
+import { Subscriptions } from 'libs/faces';
 
 const networkId = 4;
 const rpcUrl = getRpcUrl(networkId);
-const dappId = process.env.ONBOARD_API_KEY;
+const dappId = process.env.NEXT_PUBLIC_BN_ONBOARD_API_KEY;
 
-let web3;
-
-export const onboard = Onboard({
-  dappId: process.env.ONBOARD_API_KEY, // [String] The API key created by step one above
-  networkId: 4, // [Integer] The Ethereum network ID your Dapp uses.
-  subscriptions: {
-    wallet: (wallet) => {
-      // eslint-disable-next-line
-      web3 = new Web3(wallet.provider);
+export function initOnboard(subscriptions: Subscriptions) {
+  const onboard = Onboard;
+  return onboard({
+    dappId,
+    hideBranding: false,
+    networkId,
+    // darkMode: true,
+    subscriptions,
+    walletSelect: {
+      wallets: [
+        { walletName: 'metamask' },
+        {
+          walletName: 'trezor',
+          appUrl: 'https://reactdemo.blocknative.com',
+          email: 'aaron@blocknative.com',
+          rpcUrl,
+        },
+        {
+          walletName: 'ledger',
+          rpcUrl,
+        },
+        { walletName: 'authereum', disableNotifications: true },
+        {
+          walletName: 'lattice',
+          appName: 'Onboard Demo',
+          rpcUrl,
+        },
+        { walletName: 'coinbase' },
+        { walletName: 'status' },
+        { walletName: 'walletLink', rpcUrl },
+        {
+          walletName: 'portis',
+          apiKey: 'b2b7586f-2b1e-4c30-a7fb-c2d1533b153b',
+        },
+        { walletName: 'fortmatic', apiKey: 'pk_test_886ADCAB855632AA' },
+        { walletName: 'torus' },
+        { walletName: 'trust', rpcUrl },
+        {
+          walletName: 'walletConnect',
+          infuraKey: 'd5e29c9b9a9d4116a7348113f57770a8',
+        },
+        { walletName: 'opera' },
+        { walletName: 'operaTouch' },
+        { walletName: 'imToken', rpcUrl },
+        { walletName: 'meetone' },
+        { walletName: 'mykey', rpcUrl },
+        { walletName: 'wallet.io', rpcUrl },
+        { walletName: 'huobiwallet', rpcUrl },
+        { walletName: 'hyperpay' },
+        { walletName: 'atoken' },
+        { walletName: 'liquality' },
+      ],
     },
-  },
-  walletSelect: {
-    wallets: [
-      { walletName: 'metamask' },
-      {
-        walletName: 'trezor',
-        appUrl: 'https://reactdemo.blocknative.com',
-        email: 'aaron@blocknative.com',
-        rpcUrl,
-      },
-      {
-        walletName: 'ledger',
-        rpcUrl,
-      },
-      { walletName: 'authereum', disableNotifications: true },
-      {
-        walletName: 'lattice',
-        appName: 'Onboard Demo',
-        rpcUrl,
-      },
-      { walletName: 'coinbase' },
-      { walletName: 'status' },
-      { walletName: 'walletLink', rpcUrl },
-      {
-        walletName: 'portis',
-        apiKey: 'b2b7586f-2b1e-4c30-a7fb-c2d1533b153b',
-      },
-      { walletName: 'fortmatic', apiKey: 'pk_test_886ADCAB855632AA' },
-      { walletName: 'torus' },
-      { walletName: 'trust', rpcUrl },
-      {
-        walletName: 'walletConnect',
-        infuraKey: 'd5e29c9b9a9d4116a7348113f57770a8',
-      },
-      { walletName: 'opera' },
-      { walletName: 'operaTouch' },
-      { walletName: 'imToken', rpcUrl },
-      { walletName: 'meetone' },
-      { walletName: 'mykey', rpcUrl },
-      { walletName: 'wallet.io', rpcUrl },
-      { walletName: 'huobiwallet', rpcUrl },
-      { walletName: 'hyperpay' },
-      { walletName: 'atoken' },
-      { walletName: 'liquality' },
+    walletCheck: [
+      { checkName: 'derivationPath' },
+      { checkName: 'connect' },
+      { checkName: 'accounts' },
+      { checkName: 'network' },
+      { checkName: 'balance', minimumBalance: '100000' },
     ],
-  },
-  walletCheck: [
-    { checkName: 'derivationPath' },
-    { checkName: 'connect' },
-    { checkName: 'accounts' },
-    { checkName: 'network' },
-    { checkName: 'balance', minimumBalance: '100000' },
-  ],
-});
+  });
+}
 
-export function initNotify(): API {
+export function initNotify() {
   const notify = Notify;
   return notify({
     dappId,
