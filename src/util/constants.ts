1cd727ac55bc7d6f24628d1709a718f775055863
Cristopher Ortega <ortega.cpp@gmail.com>
2021-03-09 11:12:47 -0400

    Use env variables in constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,5 +1,3 @@
-// Farms
-export let WPOKT_GENESIS_FARM = '0xdb7d0a3bfcb7b34e6be510b26c55c8b60bd66d4a'
+export let WPOKT_GENESIS_FARM = process.env.REACT_APP_FARM_ADDRESS
 
-// Subgraph
-export let WPOKT_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/crisog/alpha-rinkeby-subgraph'
+export let WPOKT_SUBGRAPH_URL = process.env.REACT_APP_FARM_SUBGRAPH_URL
