a59ac9b57bade73a60925b772d6b4323f3221b3d
ECWireless <Elliott@valtechcreative.com>
2021-03-19 09:36:29 -0600

    Remove unused helper functions

--- src/libs/signer.ts
+++ src/libs/signer.ts
@@ -1,34 +1,3 @@
-import { ethers } from 'ethers';
-import Web3 from 'web3';
-
-export function getAddress(): Promise<string> {
-  // await window.ethereum.enable();
-  const provider = new ethers.providers.Web3Provider(Web3.givenProvider);
-  const signer = provider.getSigner();
-  return signer.getAddress();
-}
-
-// eslint-disable-next-line
-export async function sendTransaction(transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>) {
-  // await window.ethereum.enable();
-  const provider = new ethers.providers.Web3Provider(Web3.givenProvider);
-  const signer = provider.getSigner();
-
-  const hash = await signer.sendUncheckedTransaction(transaction);
-  return {
-    hash,
-    nonce: null,
-    gasLimit: null,
-    gasPrice: null,
-    data: null,
-    value: null,
-    chainId: null,
-    confirmations: 0,
-    from: null,
-    wait: (confirmations_1: number | undefined) => provider.waitForTransaction(hash, confirmations_1),
-  };
-}
-
 // eslint-disable-next-line
 export default function getSigner(provider: { getSigner: () => any }): any {
   const signer = provider.getSigner();
