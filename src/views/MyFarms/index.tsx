b2e5c14ecfc56a0b8a9bce264309d1c211ee0609
ECWireless <Elliott@valtechcreative.com>
2021-03-29 20:42:17 -0600

    Add stats to My Farms page

--- src/views/MyFarms/index.tsx
+++ src/views/MyFarms/index.tsx
@@ -29,11 +29,18 @@ import {
 import Spacer from 'components/Spacer';
 import { H1, P2 } from 'components/Typography';
 
+import { TOKEN_GEYSER_ADDRESS } from 'constants/index';
+
 import { DepositWithdrawalContext } from 'contexts/DepositWithdrawal';
+
+import { useFarmStats } from 'hooks/useFarmStats';
+
+import { commifyString } from 'utils';
 
 const MyFarms: React.FC = () => {
   const history = useHistory();
   const { onSetActionType } = React.useContext(DepositWithdrawalContext);
+  const { apy, timeRemaining, totalStaked } = useFarmStats(TOKEN_GEYSER_ADDRESS);
   const [farmSelected, setFarmSelected] = React.useState<boolean>(false);
 
   const onDepositWithdrawLink = (actionType: 'deposit' | 'withdraw') => {
@@ -60,7 +67,9 @@ const MyFarms: React.FC = () => {
               <div id={'estimated-reward'}>
                 <P2 color={colors.white}>Total Staked</P2>
                 <Flex align={'center'}>
-                  <StyledStakedText color={colors.white}>2,669.830235 wPOKT</StyledStakedText>
+                  <StyledStakedText color={colors.white}>
+                    {commifyString(totalStaked.toFixed(6))} wPOKT
+                  </StyledStakedText>
                   <StyledSelectorContainer farmSelected={farmSelected}>
                     <SelectorSvg />
                   </StyledSelectorContainer>
@@ -71,15 +80,28 @@ const MyFarms: React.FC = () => {
           {farmSelected && (
             <StyledSmallInfoCardsContainer>
               <StyledContentContainer>
-                <SmallInfoCard iconType={'question'} statTitle={'APY'} statContent={'10%'} />
+                <SmallInfoCard
+                  iconType={'question'}
+                  statTitle={'APY'}
+                  statContent={`${commifyString(apy.toFixed(2))}%`}
+                />
                 <SmallInfoCard iconType={'caret'} statTitle={'Multiplier'} statContent={'1.0 X'} />
-                <SmallInfoCard iconType={'question'} statTitle={'Total Staked'} statContent={'23,456.3 wpokt'} />
+                <SmallInfoCard
+                  iconType={'question'}
+                  statTitle={'Total Staked'}
+                  statContent={`${commifyString(totalStaked.toFixed(2))} wPOKT`}
+                />
                 <SmallInfoCard iconType={'caret'} statTitle={'Max Relays/Day'} statContent={'1 M'} />
                 <SmallInfoCard iconType={'question'} statTitle={'Farm Usage'} statContent={'55.4%'} />
                 <SmallInfoCard iconType={'question'} statTitle={'Supported APps'} statContent={'12'} />
                 <SmallInfoCard iconType={'question'} statTitle={'Rewards unlocked'} statContent={'30%'} />
                 <SmallInfoCard iconType={'question'} statTitle={'Farm ownership'} statContent={'14%'} />
-                <SmallInfoCard iconType={'question'} statTitle={'Duration'} statContent={'54 Days'} statFill={38} />
+                <SmallInfoCard
+                  iconType={'question'}
+                  statTitle={'Duration'}
+                  statContent={`${timeRemaining?.days} Days`}
+                  statFill={38}
+                />
                 <SmallInfoCardExtraLinks />
               </StyledContentContainer>
               <div>
