970782deac1c8b833058b6d60699e5f3cf0e0c68
ECWireless <Elliott@valtechcreative.com>
2021-05-05 15:44:21 -0600

    Add tooltip for Rewards Unlocked

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ReactTooltip from 'react-tooltip';
 import styled from 'styled-components/macro';
 import type {} from 'styled-components/cssprop';
 import GlobalFonts from 'fonts/font';
+import { GU } from 'components/theme';
 
 import Footer from 'components/Footer';
 import Modals from 'components/Modals';
